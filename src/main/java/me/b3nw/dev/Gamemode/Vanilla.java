3de4da84323b9f7986423474991f0123407e683e
Ben <ben+git@b3nw.me>
2015-06-08 23:38:22 +0100

    added local player class & methods to add and remove blobs

--- src/main/java/me/b3nw/dev/Gamemode/Vanilla.java
+++ src/main/java/me/b3nw/dev/Gamemode/Vanilla.java
@@ -4,12 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import me.b3nw.dev.Events.EventHandle;
 import me.b3nw.dev.Events.GameEvent;
 import me.b3nw.dev.Events.NickAnnounceEvent;
+import me.b3nw.dev.Session.Blobs.Blob;
 import net.sf.jsi.rtree.RTree;
+
+import java.util.HashMap;
 
 @Slf4j
 public class Vanilla implements Gamemode {
 
     private final RTree blobTree;
+    private HashMap<Integer, Blob> blobMap = new HashMap<>();
     private int lastId = 0;
 
     public Vanilla() {
@@ -28,5 +32,24 @@ public class Vanilla implements Gamemode {
     @Override
     public void runTick() {
 
+    }
+
+    private void addBlob(Blob blob) {
+        blobTree.add(blob, blob.getId());
+        blobMap.put(blob.getId(), blob);
+    }
+
+    private void removeBlob(Blob blob) {
+        blobMap.remove(blob.getId());
+        blobTree.delete(blob, blob.getId());
+    }
+
+    private class Player extends Blob {
+        public Player(int id, double x, double y, double size, String name) {
+            super(id, x, y, size);
+            this.setVirus(false);
+            this.setVisible(true);
+            this.setName(name);
+        }
     }
 }
