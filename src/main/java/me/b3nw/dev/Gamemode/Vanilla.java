24a5aeac11e61720caadf76b8d4469fb5609c930
Ben <ben+git@b3nw.me>
2015-06-08 23:33:20 +0100

    added blob quad tree

--- src/main/java/me/b3nw/dev/Gamemode/Vanilla.java
+++ src/main/java/me/b3nw/dev/Gamemode/Vanilla.java
@@ -4,9 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import me.b3nw.dev.Events.EventHandle;
 import me.b3nw.dev.Events.GameEvent;
 import me.b3nw.dev.Events.NickAnnounceEvent;
+import net.sf.jsi.rtree.RTree;
 
 @Slf4j
 public class Vanilla implements Gamemode {
+
+    private final RTree blobTree;
+    private int lastId = 0;
+
+    public Vanilla() {
+        this.blobTree = new RTree();
+        blobTree.init(null);
+
+    }
 
     @EventHandle(type = EventHandle.Type.NICKANNOUNCE)
     public void nickAnnounce(GameEvent evt) {
